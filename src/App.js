1978543483b8026f45a2158d008fcf2eaa02cd6d
Léo Delplanque <delplanqueleomail@gmail.com>
2022-11-29 11:25:23 +0900

    feat: delete animes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem("animes", JSON.stringify(animes));
+    console.log("animes", animes);
   }, [animes]);
 
   return (
--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,20 +1,35 @@
 import { Link } from "react-chrome-extension-router";
+import { AnimeContext } from "../context/AnimeContext";
 import Anime from "../pages/Anime";
+import { useContext } from "react";
 
 export default function AnimeCard({ anime }) {
+  const setAnimes = useContext(AnimeContext);
+  const deleteAnime = (id) => {
+    console.log("delete");
+    setAnimes((prevAnimes) => {
+      return prevAnimes.filter((anime) => anime.id !== id);
+    });
+  };
+
   return (
-    <Link
-      className="anime-card"
-      props={{
-        anime,
-      }}
-      component={Anime}
-    >
-      <img src={anime.img} alt={`${anime.name}`} />
-      <span>{anime.name}</span>
-      <span hidden={anime.airing}>
-        {anime.nbSeen}/{anime.nbEpisodes}
+    <div className="anime-card-container">
+      <Link
+        className="anime-card"
+        props={{
+          anime,
+        }}
+        component={Anime}
+      >
+        <img src={anime.img} alt={`${anime.name}`} />
+        <span>{anime.name}</span>
+        <span hidden={anime.airing}>
+          {anime.nbSeen}/{anime.nbEpisodes}
+        </span>
+      </Link>
+      <span className="add-btn" onClick={() => deleteAnime(anime.id)}>
+        x
       </span>
-    </Link>
+    </div>
   );
 }
